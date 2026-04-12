0031e7ca753811fe49f68c34c081210bddd33aa8
Nikhil Iyer <nrinikhil@gmail.com>
2026-04-12 00:15:26 -0400

    Orchestrate ingestion + dbt run/test in Airflow

--- dags/usajobs_ingestion_dag.py
+++ dags/usajobs_ingestion_dag.py
@@ -15,4 +15,21 @@ with DAG(
         bash_command="python /opt/airflow/ingestion/usajobs_ingest.py",
     )
 
-    ingest
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command=(
+            "dbt run "
+            "--project-dir /opt/airflow/dbt/usajobs_analytics "
+            "--profiles-dir /opt/airflow/dbt "
+        ),
+    )
+
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command=(
+            "dbt test "
+            "--project-dir /opt/airflow/dbt/usajobs_analytics "
+            "--profiles-dir /opt/airflow/dbt "
+        ),
+    )
+    ingest >> dbt_run >> dbt_test
